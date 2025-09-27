da5b9393174b75643c01fe433f3220c0bc09f3d4
Aishwarya Patil <aishwaryapatil845@gmail.com>
2025-09-27 12:50:22 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,25 @@
+
+print("aishwarya")
+print(0.3*0.9)
+print(2*2)
+print(3-2)
+print(4/2)
+print(3+4)
+print("aiml")
+a = 2
+b = 0.5
+c = a*b
+print(c)
+x = "hello"
+y = "world"
+print(x+y)
+a = True 
+print(a)
+print(type(a))
+print(type(b))
+print(type(x))
+a=5
+print(f"number a = {a}")
+print(f"number b = {b}")
+age = input("what is your age ")
+print(f"your current age is {age}\n")
